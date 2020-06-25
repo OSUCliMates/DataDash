94e7e678c4c7143e919fbd422b31290ae67f3899
palmerem <palmerem@rstudio-4.cosine.oregonstate.edu>
2020-06-24 22:44:21 -0700

    change layout and add state filter by mle

--- server.R
+++ server.R
@@ -1,48 +1,66 @@
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-  output$sawtooth <- renderPlot({
-    a <- input$lat[1]
-    b <- input$lat[2]
-    c <- input$lon[1]%%360
-    d <- input$lon[2]%%360
-    
-    plot_sawtooths(a,b,c,d)
-  })
-  
-  output$ref_map <- renderPlot({
-    ggplot(us, aes(x = long, y = lat, group = group)) +
-      geom_polygon(fill="lightgray", colour = "black")+
-      geom_rect(aes(xmin=input$lon[1],
-                    xmax=input$lon[2],
-                    ymin=input$lat[1],
-                    ymax=input$lat[2]),
-                fill="darkorchid2",
-                alpha=0.01
-      )
-    
-  })
   
   # K8 I think
     output$sawtooth <- renderPlot({
-      a <- input$lat[1]
-      b <- input$lat[2]
-      c <- input$lon[1]%%360
-      d <- input$lon[2]%%360
+      #a <- input$lat[1]
+      #b <- input$lat[2]
+      #c <- input$lon[1]%%360
+      #d <- input$lon[2]%%360
+      # figure out what to do if inf 
       
+      max_mins <- brushedPoints(location_points,
+                                input$plot_brush,
+                                xvar = "lon",yvar = "lat") %>% 
+        filter(dataset == "lens") %>% 
+        summarize(min_lon = min(lon),
+                  max_lon = max(lon),
+                  min_lat = min(lat),
+                  max_lat = max(lat)) %>% 
+        as_vector()
+      a <- max_mins[3]
+      b <- max_mins[4]
+      c <- max_mins[1]%%360
+      d <- max_mins[2]%%360
       plot_sawtooths(a,b,c,d)
+
     })
     
     output$ref_map <- renderPlot({
+      
+      max_mins <- brushedPoints(location_points,
+                                input$plot_brush,
+                                xvar = "lon",yvar = "lat") %>% 
+        filter(dataset == "lens") %>% 
+        summarize(min_lon = min(lon),
+                  max_lon = max(lon),
+                  min_lat = min(lat),
+                  max_lat = max(lat))
+      a <- max_mins[1,3]
+      b <- max_mins[1,4]
+      c <- max_mins[1,1]
+      d <- max_mins[1,2]
+      
       ggplot(us, aes(x = long, y = lat, group = group)) +
         geom_polygon(fill="lightgray", colour = "black")+
-        geom_rect(aes(xmin=input$lon[1],
-                      xmax=input$lon[2],
-                      ymin=input$lat[1],
-                      ymax=input$lat[2]),
+        geom_rect(aes(xmin=c,
+                      xmax=d,
+                      ymin=a,
+                      ymax=b),
                   fill="darkorchid2",
                   alpha=0.01
-                  )
+        )
+      
+      
+      #ggplot(us, aes(x = long, y = lat, group = group)) +
+      #  geom_polygon(fill="lightgray", colour = "black")+
+      #  geom_rect(aes(xmin=input$lon[1],
+      #                xmax=input$lon[2],
+      #                ymin=input$lat[1],
+      #                ymax=input$lat[2]),
+      #            fill="darkorchid2",
+      #            alpha=0.01
+      #            )
       
     })
     # smither8
@@ -77,6 +95,71 @@ server <- function(input, output) {
         geom_point(aes(x=Longitude, y=Latitude))
     }
     )
+    
+    
+    
+    
+    ## MLE 
+    
+    
+    location_points <- read.csv("~/CliMates/Output/lat_lon_pairs.csv")
+    
+    zoom_val <- reactiveVal(0)       # rv <- reactiveValues(value = 0)
+    
+    observeEvent(input$zoom_in, {
+      if(zoom_val() -1 < 0){
+        newValue <- zoom_val() - .1
+      }
+      else{
+        newValue <- zoom_val()-1
+      }
+      zoom_val(newValue)
+    })
+    
+    observeEvent(input$zoom_out, {
+      if(zoom_val() < 0){
+        newValue <- zoom_val()+.1
+      }
+      else{
+        newValue <- zoom_val() + 1
+      } 
+      zoom_val(newValue) 
+    })
+    observeEvent(input$state,{
+      zoom_val(0)
+    })
+    observeEvent(input$reset,{
+      zoom_val(0)
+    })
+    bounding <- reactive({
+      state_map <- map_data("state",region = input$state)
+      list(lon_range = c(max(-136.5,min(state_map$long)-zoom_val()),
+                         min(-58.5,max(state_map$long)+zoom_val())),
+           lat_range = c(max(17.25,min(state_map$lat)-zoom_val()),
+                         min(55.5,max(state_map$lat)+zoom_val())))
+    })
+    
+    output$point_selection_map <- renderPlot({
+      mapdata <- map_data("state")
+      if(zoom_val()<0){
+        mapdata <- map_data("county")
+      }
+      ggplot() + 
+        geom_polygon(data = mapdata,
+                     aes(x=long,y=lat,group=group), fill = NA, color = "black")+
+        geom_point(data = location_points, 
+                   aes(x = lon, y = lat,color = dataset)) +
+        coord_cartesian(xlim = bounding()$lon_range,
+                        ylim = bounding()$lat_range)
+    })
+    
+
+    # need to figure out good error message if nothing selected? 
+    max_min_locations <- reactiveValues(min_lat = 41, 
+                                        max_lat = 47,
+                                        min_lon = -125,
+                                        max_lon = -116)
+    
 
 }
 
